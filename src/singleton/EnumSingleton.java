17678c52de744d3d66e4b6dfb8432120ff4d598b
Silviu <freemanfx@gmail.com>
2013-11-27 00:38:49 +0200

    added Singleton pattern examples

---
+++ src/singleton/EnumSingleton.java
@@ -0,0 +1,11 @@
+package singleton;
+
+import abstractfactory.CarFactory;
+
+import java.awt.*;
+import java.util.Iterator;
+
+public enum EnumSingleton
+{
+    ENUM1
+}
---
+++ src/singleton/StaticConstant.java
@@ -0,0 +1,8 @@
+package singleton;
+
+public class StaticConstant {
+    public static StaticConstant INSTANCE= new StaticConstant();
+    private StaticConstant(){
+
+    }
+}
